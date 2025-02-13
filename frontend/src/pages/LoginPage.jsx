90bb642d9a44004c00e84b90dfed1290f6d4b4a7
natnael taye <natitaye316@gmail.com>
2025-02-13 22:55:24 +0300

    still in progress

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
+
+
+const LoginPage = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <motion.div initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+    >Loginpage</motion.div>
+      );
+}
+ 
+export default LoginPage;
